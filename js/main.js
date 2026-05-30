beadf6e0da2b4dbd4ae97bced758b8c23c82c8f8
AntonFomychov <anton.a.fomichov@gmail.com>
2026-05-30 16:33:21 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+	function showModal(modalId) {
+		$(".close-bg").addClass("active");
+		$("#" + modalId).addClass("active");
+	}
+
+	function closeAllModals() {
+		$(".close-bg").removeClass("active");
+		$(".modal").removeClass("active");
+	}
+
+	$(document).on("click", "[data-modal]", function (e) {
+		const modalId = $(this).data("modal");
+		closeAllModals();
+		showModal(modalId);
+	});
+
+	$(document).on("submit", "form[data-success-modal]", function (e) {
+		e.preventDefault();
+
+		if (this.checkValidity && !this.checkValidity()) {
+			this.reportValidity && this.reportValidity();
+			return;
+		}
+
+		const successModalId = $(this).data("success-modal");
+		if (!successModalId) {
+			return;
+		}
+
+		closeAllModals();
+		showModal(successModalId);
+	});
+
+	$(document).on("click", "[data-close]", function (e) {
+		e.preventDefault();
+		closeAllModals();
+	});
+
+	$(document).on("click", ".close-bg", function (e) {
+		if (e.target === this) {
+			closeAllModals();
+		}
+	});
+
+	$(document).on("keydown", function (e) {
+		if (e.key === "Escape") {
+			closeAllModals();
+		}
+	});
+
+	$(window).scroll(function stickyMenu() {
+	  	var fx = $(window).scrollTop();
+
+	  	if (fx > 100) {
+	    	$('.header-wrap').addClass('fixed');
+	  	} else {
+	    	$('.header-wrap').removeClass('fixed');
+	  	}
+	})
+});
