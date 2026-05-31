c2e777faa96fb563c4dfe28e97423f6d77b47463
AntonFomychov <anton.a.fomichov@gmail.com>
2026-05-31 19:05:18 +0200

    delivery section + faq desktop

--- js/main.js
+++ js/main.js
@@ -63,4 +63,40 @@ $(document).ready(function () {
 	  $('.about-us-item').removeClass('active');
 	  $(this).addClass('active');
 	});
+
+	$('.delivery-bottom-item-wrap').on('mouseenter', function() {
+	  $('.delivery-bottom-item-wrap').removeClass('active');
+	  $(this).addClass('active');
+	});
+
+	$(".faq-item-headline__icon").on("click", function () {
+		const $faqItem = $(this).closest(".faq-item");
+		const $content = $faqItem.find(".faq-item-content");
+
+		$faqItem.toggleClass("active");
+		$content.stop(true, true).slideToggle(300);
+	});
+
+	$('.faq-slider').each(function() {
+	    new Swiper(this, {
+	        slidesPerView: 1,
+	        speed: 1000,
+	        navigation: {
+	            nextEl: $(this).closest('.faq-item-content').find('.slider-arrow--next')[0],
+	            prevEl: $(this).closest('.faq-item-content').find('.slider-arrow--prev')[0],
+	        },
+	        breakpoints: {
+	            501: { slidesPerView: 2 },
+	            801: { slidesPerView: 4 },
+	        },
+	        on: {
+	            sliderFirstMove() {
+	                $(this.el).addClass('is-dragging');
+	            },
+	            touchEnd() {
+	                $(this.el).removeClass('is-dragging');
+	            },
+	        }
+	    });
+	});
 });
